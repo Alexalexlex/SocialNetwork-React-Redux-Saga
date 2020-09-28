173a28c588b039420aa201d85ee91fc84dae9d58
Alex <flalex855@gmail.com>
2020-09-28 15:10:10 +0300

    change all to classes Components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
@@ -10,15 +10,15 @@ import Link from '@material-ui/core/Link'
 import Button from '@material-ui/core/Button'
 import Icon from '@material-ui/core/Icon';
 
-const useStyles = makeStyles((theme) => ({
+const classes = {
     root: {
         flexGrow: 1,
         overflow: 'hidden',
     },
     paper: {
         maxWidth: '80%',
-        margin: `${theme.spacing(1)}px auto`,
-        padding: theme.spacing(2),
+        margin: `10px auto`,
+        padding: '1rem',
     },
     input: {
         width: '80%',
@@ -30,13 +30,13 @@ const useStyles = makeStyles((theme) => ({
         cursor: 'pointer',
     },
     button: {
-        margin: theme.spacing(1),
+        margin: '1rem',
         width: '10rem',
     },
     grid: {
-        margin: `${theme.spacing(4)}px auto`,
+        margin: `10px auto`,
     },
-}));
+};
 
 
 
@@ -50,29 +50,28 @@ laboriosam culpa odio ad consequatur neque odit laudantium, corporis nobis, sit 
 adipisicing elit. Omnis explicabo maxime atque perferendis optio, laboriosam culpa odio ad consequatur neque odit laudantium, corporis nobis, 
 sit aut assumenda doloribus cumque beatae.`;
 
-export default function AutoGridNoWrap() {
-    const classes = useStyles();
-
+class Main extends React.Component {
+    render() {
     return (
-        <div className={classes.root}>
+        <div className={this.props.classes.root}>
             <MenuNav />
             <form>
             <Grid
                 container
-                className={classes.grid}
+                className={this.props.classes.grid}
                 direction="column"
                 justify="flex-start"
                 alignItems="flex-end"
                 alignContent="center"
             >
                 <TextField
-                    className={classes.input}
+                    className={this.props.classes.input}
                     label="Heading"
                     autoComplete="current-password"
                     variant="outlined"
                 />
                 <TextField
-                    className={classes.input}
+                    className={this.props.classes.input}
                     id="outlined-textarea"
                     label="Description"
                     multiline
@@ -83,7 +82,7 @@ export default function AutoGridNoWrap() {
                         type="submit"
                         variant="contained"
                         color="primary"
-                        className={classes.button}
+                        className={this.props.classes.button}
                         endIcon={<Icon>send</Icon>}
                     >
                         Send
@@ -91,8 +90,8 @@ export default function AutoGridNoWrap() {
                 </Grid>
             </Grid>
             </form>
-            <Link to="#" className={classes.link}>
-                <Paper className={classes.paper}>
+            <Link to="#" className={this.props.classes.link}>
+                <Paper className={this.props.classes.paper}>
                     <Grid container wrap="nowrap" spacing={2}>
                         <Grid item>
                             <Avatar>W</Avatar>
@@ -103,7 +102,7 @@ export default function AutoGridNoWrap() {
                     </Grid>
                 </Paper>
             </Link>
-            <Paper className={classes.paper}>
+            <Paper className={this.props.classes.paper}>
                 <Grid container wrap="nowrap" spacing={2}>
                     <Grid item>
                         <Avatar>W</Avatar>
@@ -113,7 +112,7 @@ export default function AutoGridNoWrap() {
                     </Grid>
                 </Grid>
             </Paper>
-            <Paper className={classes.paper}>
+            <Paper className={this.props.classes.paper}>
                 <Grid container wrap="nowrap" spacing={2}>
                     <Grid item>
                         <Avatar>W</Avatar>
@@ -123,7 +122,7 @@ export default function AutoGridNoWrap() {
                     </Grid>
                 </Grid>
             </Paper>
-            <Paper className={classes.paper}>
+            <Paper className={this.props.classes.paper}>
                 <Grid container wrap="nowrap" spacing={2}>
                     <Grid item>
                         <Avatar>W</Avatar>
@@ -133,7 +132,7 @@ export default function AutoGridNoWrap() {
                     </Grid>
                 </Grid>
             </Paper>
-            <Paper className={classes.paper}>
+            <Paper className={this.props.classes.paper}>
                 <Grid container wrap="nowrap" spacing={2}>
                     <Grid item>
                         <Avatar>W</Avatar>
@@ -143,7 +142,7 @@ export default function AutoGridNoWrap() {
                     </Grid>
                 </Grid>
             </Paper>
-            <Paper className={classes.paper}>
+            <Paper className={this.props.classes.paper}>
                 <Grid container wrap="nowrap" spacing={2}>
                     <Grid item>
                         <Avatar>W</Avatar>
@@ -153,7 +152,7 @@ export default function AutoGridNoWrap() {
                     </Grid>
                 </Grid>
             </Paper>
-            <Paper className={classes.paper}>
+            <Paper className={this.props.classes.paper}>
                 <Grid container wrap="nowrap" spacing={2}>
                     <Grid item>
                         <Avatar>W</Avatar>
@@ -166,3 +165,6 @@ export default function AutoGridNoWrap() {
         </div>
     );
 }
+}
+
+export default withStyles(classes)(Main)
